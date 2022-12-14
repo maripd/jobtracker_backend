c3f1b1f9333d22437a35c93aacaa064ad41e189f
Marissa Dilig <hello.maripd@gmail.com>
2022-12-14 13:01:18 -0600

    added app.use server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 // app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', routes)
